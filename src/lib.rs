81db7069d9d02d3acea2a8f9608a43f370d8cd8a
Linden <70739041+linden@users.noreply.github.com>
2022-06-22 20:01:41 -0700

    style!: simplify `create_*_from` to `create_from`

--- src/lib.rs
+++ src/lib.rs
@@ -39,7 +39,7 @@ pub unsafe extern "C" fn create_cstr(source: Box<&str>) -> *mut libc::c_char {
 }
 
 #[no_mangle]
-pub extern "C" fn create_uint_from_u8(source: Box<u8>) -> *mut libc::c_uint {
+pub extern "C" fn create_from_u8(source: Box<u8>) -> *mut libc::c_uint {
     (*source) as *mut libc::c_uint
 }
 
@@ -49,7 +49,7 @@ pub extern "C" fn create_u8(source: *mut libc::c_uint) -> Box<u8> {
 }
 
 #[no_mangle]
-pub extern "C" fn create_uint_from_u16(source: Box<u16>) -> *mut libc::c_uint {
+pub extern "C" fn create_from_u16(source: Box<u16>) -> *mut libc::c_uint {
     (*source) as *mut libc::c_uint
 }
 
@@ -59,7 +59,7 @@ pub extern "C" fn create_u16(source: *mut libc::c_uint) -> Box<u16> {
 }
 
 #[no_mangle]
-pub extern "C" fn create_uint_from_u32(source: Box<u32>) -> *mut libc::c_uint {
+pub extern "C" fn create_from_u32(source: Box<u32>) -> *mut libc::c_uint {
     (*source) as *mut libc::c_uint
 }
 
@@ -69,7 +69,7 @@ pub extern "C" fn create_u32(source: *mut libc::c_uint) -> Box<u32> {
 }
 
 #[no_mangle]
-pub extern "C" fn create_ulong_from_u64(source: Box<u64>) -> *mut libc::c_ulong {
+pub extern "C" fn create_from_u64(source: Box<u64>) -> *mut libc::c_ulong {
     (*source) as *mut libc::c_ulong
 }
 
@@ -79,7 +79,7 @@ pub extern "C" fn create_u64(source: *mut libc::c_ulong) -> Box<u64> {
 }
 
 #[no_mangle]
-pub extern "C" fn create_size_from_usize(source: Box<usize>) -> *mut libc::size_t {
+pub extern "C" fn create_from_usize(source: Box<usize>) -> *mut libc::size_t {
     (*source) as *mut libc::size_t
 }
 
@@ -89,7 +89,7 @@ pub extern "C" fn create_usize(source: *mut libc::size_t) -> Box<usize> {
 }
 
 #[no_mangle]
-pub extern "C" fn create_int_from_i8(source: Box<i8>) -> *mut libc::c_int {
+pub extern "C" fn create_from_i8(source: Box<i8>) -> *mut libc::c_int {
     (*source) as *mut libc::c_int
 }
 
@@ -99,7 +99,7 @@ pub extern "C" fn create_i8(source: *mut libc::c_int) -> Box<i8> {
 }
 
 #[no_mangle]
-pub extern "C" fn create_int_from_i16(source: Box<i16>) -> *mut libc::c_int {
+pub extern "C" fn create_from_i16(source: Box<i16>) -> *mut libc::c_int {
     (*source) as *mut libc::c_int
 }
 
@@ -109,7 +109,7 @@ pub extern "C" fn create_i16(source: *mut libc::c_int) -> Box<i16> {
 }
 
 #[no_mangle]
-pub extern "C" fn create_int_from_i32(source: Box<i32>) -> *mut libc::c_int {
+pub extern "C" fn create_from_i32(source: Box<i32>) -> *mut libc::c_int {
     (*source) as *mut libc::c_int
 }
 
@@ -119,7 +119,7 @@ pub extern "C" fn create_i32(source: *mut libc::c_int) -> Box<i32> {
 }
 
 #[no_mangle]
-pub extern "C" fn create_long_from_i64(source: Box<i64>) -> *mut libc::c_long {
+pub extern "C" fn create_from_i64(source: Box<i64>) -> *mut libc::c_long {
     (*source) as *mut libc::c_long
 }
 
@@ -129,7 +129,7 @@ pub extern "C" fn create_i64(source: *mut libc::c_long) -> Box<i64> {
 }
 
 #[no_mangle]
-pub extern "C" fn create_ssize_from_isize(source: Box<isize>) -> *mut libc::ssize_t {
+pub extern "C" fn create_from_isize(source: Box<isize>) -> *mut libc::ssize_t {
     (*source) as *mut libc::ssize_t
 }
 
